c952c3cb3ae52f25e16b8d7be8ae2b4f1131f7ed
Zachary driskill <zacharydriskill@Zacharys-MacBook-Air-99.local>
2022-11-12 12:07:08 -0700

    updated state machine

--- PlatformIORoboticsComp/src/main.cpp
+++ PlatformIORoboticsComp/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <HCSR04.h>
+
 // Pin Assignments
 const int interruptPinR = 3;
 const int interruptPinL = 2;
@@ -119,10 +120,12 @@ const byte triggerPin = 6;
 const byte echoPin = 5;
 UltraSonicDistanceSensor distanceSensor(triggerPin, echoPin);
 
-float leftWallDistance(int target){
+float leftWallDistance(int target)
+{
     // Every 500 miliseconds, do a measurement using the sensor and print the distance in centimeters.
     float distance = distanceSensor.measureDistanceCm();
-    if (distance != -1 && distance < 40){
+    if (distance != -1 && distance < 40)
+    {
         float difference = distance - target;
         // Serial.println(difference);
         return difference;
@@ -155,7 +158,8 @@ void ultrasonicAdjust(int target)
     }
 }
 
-void ultrasonic() {
+void ultrasonic()
+{
     static int speedL = 100;
     static int speedR = 100;
     float distance = distanceSensor.measureDistanceCm();
@@ -163,7 +167,6 @@ void ultrasonic() {
     {
         speedL = 90;
         speedR = 125;
-        
     }
     else if (distance < 15)
     {
@@ -179,7 +182,8 @@ void ultrasonic() {
     analogWrite(motorR, speedR);
 }
 
-void setup() {
+void setup()
+{
     Serial.begin(9600);
     // pin modes and interrupts
     pinMode(buttonBackPin, INPUT_PULLUP);
@@ -198,43 +202,138 @@ void setup() {
     analogWrite(motorL, motorLS);
 }
 
-void statemachine(){
-static int state = 0;
-static int buttonFront = 0;
-static int buttonBack = 0;
-buttonFront = digitalRead(buttonFrontPin);
-buttonBack = digitalRead(buttonBackPin);
-if((buttonBack == 0) && (buttonFront == 1))
-    state = 1;
-else if((buttonBack == 1) && (buttonFront == 0))
-    state = 2;
-else if((buttonBack == 0) && (buttonFront == 0))
-    state = 3;
-else
-    state = 0;
-switch (state) {
-case 0:         //both buttons pressed OK
-    motorLS = 155;
-    motorRS = 159;
-    break;
-case 1:         // the back button is not pushed increase left motor speed
-    motorLS = 155;
-    motorRS = 155;
-    break;
-case 2:             //the front button is not pushed increase right motor speed
-    motorLS = 120;
-    motorRS = 155;
-    break;
-case 3:             //both buttons not pushed increase right motor speed
-    motorLS = 90;
-    motorRS = 190;
-    break;
-}
-analogWrite(motorR, motorRS);
-analogWrite(motorL, motorLS);
-Serial.println(state);
+// void statemachine()
+// {
+//     static int state = 0;
+//     static int buttonFront = 0;
+//     static int buttonBack = 0;
+//     buttonFront = digitalRead(buttonFrontPin);
+//     buttonBack = digitalRead(buttonBackPin);
+//     if ((buttonBack == 0) && (buttonFront == 1))
+//         state = 1;
+//     else if ((buttonBack == 1) && (buttonFront == 0))
+//         state = 2;
+//     else if ((buttonBack == 0) && (buttonFront == 0))
+//         state = 3;
+//     else
+//         state = 0;
+//     switch (state)
+//     {
+//     case 0: // both buttons pressed OK
+//         motorLS = 155;
+//         motorRS = 159;
+//         break;
+//     case 1: // the back button is not pushed increase left motor speed
+//         motorLS = 155;
+//         motorRS = 155;
+//         break;
+//     case 2: // the front button is not pushed increase right motor speed
+//         motorLS = 120;
+//         motorRS = 155;
+//         break;
+//     case 3: // both buttons not pushed increase right motor speed
+//         motorLS = 90;
+//         motorRS = 190;
+//         break;
+//     }
+//     analogWrite(motorR, motorRS);
+//     analogWrite(motorL, motorLS);
+//     Serial.println(state);
+// }
+
+// Define SM states
+typedef enum
+{
+    START,
+    GO_TO_WALL,
+    STRAIGHT_PUSH_BACK_BTN,
+    STRAIGHT_PUSH_FRONT_BTN,
+    STRAIGHT_MAINTAIN,
+    TURN_PUSH_FRONT_BTN,
+    TURN_MAINTAIN
+} sm_state_t;
+
+// SM Variables
+sm_state_t currentState;
+
+void SM_init()
+{
+    currentState = GO_TO_WALL;
 }
 
-void loop(){
+void SM_tick()
+{
+    // Read Values
+    static int buttonFront = 0;
+    static int buttonBack = 0;
+    buttonFront = digitalRead(buttonFrontPin);
+    buttonBack = digitalRead(buttonBackPin);
+
+    // SM Transitions
+    switch (currentState)
+    {
+    case START:
+        currentState = GO_TO_WALL;
+        break;
+    case GO_TO_WALL:
+        if (buttonFront)
+            currentState = STRAIGHT_PUSH_BACK_BTN;
+        break;
+    case STRAIGHT_PUSH_BACK_BTN:
+        if (gryo_isTurning())
+            currentState = TURN_PUSH_FRONT_BTN;
+        else if (buttonBack)
+            currentState = STRAIGHT_MAINTAIN;
+        break;
+    case STRAIGHT_PUSH_FRONT_BTN:
+        if (gyro_isTurning())
+            currentState = TURN_PUSH_FRONT_BTN;
+        else if (buttonFront)
+            currentState = STRAIGHT_MAINTAIN;
+        break;
+    case STRAIGHT_MAINTAIN:
+        if (gyro_isTurning())
+            currentState = TURN_PUSH_FRONT_BTN;
+        else if (!buttonFront)
+            currentState = STRAIGHT_PUSH_FRONT_BTN;
+        else if (!buttonBack)
+            currentState = STRAIGHT_PUSH_BACK_BTN;
+        break;
+    case TURN_PUSH_FRONT_BTN:
+        if (!gyro_isTurning())
+            currentState = STRAIGHT_MAINTAIN;
+        else if (buttonFront)
+            currentState = TURN_MAINTAIN;
+        break;
+    case TURN_MAINTAIN:
+        if (!gyro_isTurning())
+            currentState = STRAIGHT_MAINTAIN;
+        else if (!buttonFront)
+            currentState = TURN_PUSH_FRONT_BTN;
+        break;
+    }
+
+    // SM Actions
+    switch (currentState)
+    {
+    case START:
+        break;
+    case GO_TO_WALL:
+        break;
+    case STRAIGHT_PUSH_FRONT_BTN:
+        break;
+    case STRAIGHT_PUSH_BACK_BTN:
+        break;
+    case STRAIGHT_MAINTAIN:
+        break;
+    case TURN_PUSH_FRONT_BTN:
+        break;
+    case TURN_MAINTAIN:
+        break;
+    }
+}
+
+void loop()
+{
     statemachine();
 }
